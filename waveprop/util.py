56f52d67dc6981d1e989d45c0ebb92bc2e0fca9a
Eric B <eric.bezzam@epfl.ch>
2021-06-22 10:41:31 +0200

    Add plotting utility.

--- waveprop/util.py
+++ waveprop/util.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.fft import fftshift, fft2, ifftshift, ifft2
 from scipy.special import j1
+import matplotlib.pyplot as plt
 
 
 def ft2(g, delta):
@@ -142,3 +143,30 @@ def jinc(x):
     idx = x != 0
     y[idx] = 2.0 * j1(np.pi * x[idx]) / (np.pi * x[idx])
     return y
+
+
+def plot2d(x_vals, y_vals, Z, pcolormesh=True, colorbar=True):
+
+    if pcolormesh:
+        # define corners of mesh
+        dx = x_vals[1] - x_vals[0]
+        x_vals -= dx / 2
+        x_vals = np.append(x_vals, [x_vals[-1] + dx])
+
+        dy = y_vals[1] - y_vals[0]
+        y_vals -= dy / 2
+        y_vals = np.append(y_vals, [y_vals[-1] + dy])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    X, Y = np.meshgrid(x_vals, y_vals)
+    if pcolormesh:
+        cp = ax.pcolormesh(X, Y, Z.T)
+    else:
+        cp = ax.contourf(X, Y, Z.T)
+    fig = plt.gcf()
+    if colorbar:
+        fig.colorbar(cp, ax=ax, orientation="vertical")
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
+    return ax
