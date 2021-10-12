74def229201df118187d37477342bd048020c63d
Eric Bezzam <ebezzam@gmail.com>
2021-10-12 19:38:48 +0200

    Try to vectorize shifted apertures.

--- examples/superimpose_apertures.py
+++ examples/superimpose_apertures.py
@@ -22,7 +22,8 @@ plot_int = False  # or amplitude
 pyffs = False  # doesn't make a difference if same input/output resolution
 n_wavelength = 10
 dz = 5e-3
-pixel_grid = [3, 3]
+pixel_grid = [23, 9]
+# pixel_grid = [2, 2]
 
 
 d1 = L / N  # source-plane grid spacing
@@ -75,39 +76,39 @@ print(f"Computation time: {time.time() - start_time}")
 plot2d(x2, y2, u_out_combined, title="BLAS {} m".format(dz))
 
 
-""" simulate apertures individually """
-u_out = np.zeros((len(cs.wv),) + u_in.shape, dtype=np.float32)
-bar = progressbar.ProgressBar()
-start_time = time.time()
-for i in bar(range(cs.n_wavelength)):
-    u_out_wv = np.zeros((len(y1), x1.shape[1]), dtype=np.complex64)
-
-    # first aperture
-    u_out_wv_1, x2, y2 = angular_spectrum(
-        u_in=u_in_1 * gain, wv=cs.wv[i], d1=d1, dz=dz, pyffs=pyffs
-    )
-    u_out_wv_2, _, _ = angular_spectrum(u_in=u_in_2 * gain, wv=cs.wv[i], d1=d1, dz=dz, pyffs=pyffs)
-    u_out_wv = u_out_wv_1 + u_out_wv_2
-
-    if plot_int:
-        res = np.real(u_out_wv * np.conjugate(u_out_wv))
-    else:
-        res = np.abs(u_out_wv)
-    u_out[i] = res
-
-# convert to RGB
-rgb = cs.to_rgb(u_out, clip=True)
-
-# gamma correction
-rgb = gamma_correction(rgb, gamma=2.4)
-
-# reshape back
-u_out_super = (rgb.T).reshape((u_in.shape[0], u_in.shape[1], 3))
-
-print(f"Computation time: {time.time() - start_time}")
-
-plot2d(x2, y2, u_out_super, title="BLAS {} m, superimposed".format(dz))
-
+# """ simulate apertures individually """
+# u_out = np.zeros((len(cs.wv),) + u_in.shape, dtype=np.float32)
+# bar = progressbar.ProgressBar()
+# start_time = time.time()
+# for i in bar(range(cs.n_wavelength)):
+#     u_out_wv = np.zeros((len(y1), x1.shape[1]), dtype=np.complex64)
+#
+#     # first aperture
+#     u_out_wv_1, x2, y2 = angular_spectrum(
+#         u_in=u_in_1 * gain, wv=cs.wv[i], d1=d1, dz=dz, pyffs=pyffs
+#     )
+#     u_out_wv_2, _, _ = angular_spectrum(u_in=u_in_2 * gain, wv=cs.wv[i], d1=d1, dz=dz, pyffs=pyffs)
+#     u_out_wv = u_out_wv_1 + u_out_wv_2
+#
+#     if plot_int:
+#         res = np.real(u_out_wv * np.conjugate(u_out_wv))
+#     else:
+#         res = np.abs(u_out_wv)
+#     u_out[i] = res
+#
+# # convert to RGB
+# rgb = cs.to_rgb(u_out, clip=True)
+#
+# # gamma correction
+# rgb = gamma_correction(rgb, gamma=2.4)
+#
+# # reshape back
+# u_out_super = (rgb.T).reshape((u_in.shape[0], u_in.shape[1], 3))
+#
+# print(f"Computation time: {time.time() - start_time}")
+#
+# plot2d(x2, y2, u_out_super, title="BLAS {} m, superimposed".format(dz))
+#
 """ shift same aperture in the frequency domain """
 u_in_cent = rect2d(x1, y1, diam)
 u_out = np.zeros((len(cs.wv),) + u_in.shape, dtype=np.float32)
@@ -120,7 +121,7 @@ for i in bar(range(cs.n_wavelength)):
         d1=d1,
         dz=dz,
         pyffs=pyffs,
-        in_shift=[[0, diam[1]], [0, -diam[1]]],
+        in_shift=centers,
     )
 
     if plot_int:
@@ -147,13 +148,13 @@ plot2d(x2, y2, u_out_four, title="BLAS {} m, superimposed Fourier".format(dz))
 # u_out_combined /= np.max(u_out_combined)
 # u_out_super /= np.max(u_out_super)
 # u_out_four /= np.max(u_out_four)
-err = np.linalg.norm(u_out_combined - u_out_super)
-print(f"Error, superimposed : {err}")
-err = np.linalg.norm(u_out_combined - u_out_four)
-print(f"Error, superimposed Fourier : {err}")
+# err = np.linalg.norm(u_out_combined - u_out_super)
+# print(f"Error, superimposed : {err}")
+err = np.linalg.norm(u_out_combined - u_out_four) / np.linalg.norm(u_out_combined)
+print(f"Normalized error : {err}")
 
 print(u_out_combined.max())
-print(u_out_super.max())
+# print(u_out_super.max())
 print(u_out_four.max())
 
 plt.show()
