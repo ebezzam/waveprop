1ec33812317c6f563ec045263fbe7e9f3e37a204
Benoit Roussel <btroussel@gmail.com>
2022-07-13 11:20:18 +0200

    add get_slm_mask_separable()

--- waveprop/slm.py
+++ waveprop/slm.py
@@ -129,28 +129,28 @@ def get_slm_mask(
                 cf = torch.tensor(cf).to(slm_vals_flat)
             return cf * slm_vals_flat, centers
 
+        _height_pixel, _width_pixel = (slm_config[SLMParam.CELL_SIZE] / d1).astype(int)
+
         for i, _center in enumerate(centers):
 
             _center_pixel = (_center / d1 + target_dim / 2).astype(int)
-            _height_pixel, _width_pixel = (slm_config[SLMParam.CELL_SIZE] / d1).astype(int)
+            _center_top_left_pixel = (
+                _center_pixel[0] - np.floor(_height_pixel / 2).astype(int),
+                _center_pixel[1] + 1 - np.floor(_width_pixel / 2).astype(int),
+            )
 
-            rect = np.tile(cf[:, i][:, np.newaxis, np.newaxis], (1, _height_pixel, _width_pixel))
+            # Let the possibility to have non-RGB filter
+            _rect = np.tile(cf[:, i][:, np.newaxis, np.newaxis], (1, _height_pixel, _width_pixel))
 
             if pytorch:
-                rect = torch.tensor(rect).to(slm_vals_flat)
+                _rect = torch.tensor(_rect).to(slm_vals_flat)
 
             mask[
                 :,
-                _center_pixel[0]
-                - np.floor(_height_pixel / 2).astype(int) : _center_pixel[0]
-                + np.ceil(_height_pixel / 2).astype(int),
-                _center_pixel[1]
-                + 1
-                - np.floor(_width_pixel / 2).astype(int) : _center_pixel[1]
-                + 1
-                + np.ceil(_width_pixel / 2).astype(int),
+                _center_top_left_pixel[0] : _center_top_left_pixel[0] + _height_pixel,
+                _center_top_left_pixel[1] : _center_top_left_pixel[1] + _width_pixel,
             ] = (
-                slm_vals_flat[i] * rect
+                slm_vals_flat[i] * _rect
             )
 
     else:
@@ -294,3 +294,254 @@ def get_centers(slm_dim, pixel_pitch, return_color_filter=False, first_color=0):
         return centers, cf.reshape(3, -1)
     else:
         return centers
+
+
+def get_centers_separable(slm_dim, pixel_pitch, return_color_filter=False, first_color=0):
+    """
+    Return
+
+    Parameters
+    ----------
+    slm_dim : array_like
+        Dimensions of SLM in number of pixels (Ny, Nx).
+    pixel_pitch : array_like
+        Spacing between each pixel along each dimension.
+    return_color_filter : bool
+        Whether to return color filter for each center.
+    first_color : int
+        Which color is first row. R:0, G:1, or B:2.
+
+    Returns
+    -------
+    centers_separable : tuple
+        ((Nx, 2), (Ny, 2)) array of SLM pixel centers.
+    """
+    assert len(slm_dim) == 2
+    assert len(pixel_pitch) == 2
+
+    centers_y = np.arange(slm_dim[0])[::-1, np.newaxis] * pixel_pitch[0]
+    centers_y -= np.mean(centers_y)
+    centers_x = np.arange(slm_dim[1])[np.newaxis, ::-1] * pixel_pitch[1]
+    centers_x -= np.mean(centers_x)
+    centers = (centers_y, centers_x)
+
+    if return_color_filter:
+        cf_col = np.zeros((3, slm_dim[0], 1), dtype=np.float32)
+        cf_row = np.ones((3, 1, slm_dim[1]), dtype=np.float32)
+        for i in range(slm_dim[0]):
+            cf_col[(i + first_color) % 3, i] = 1
+        return centers, [cf_col, cf_row]
+    else:
+        return centers
+
+
+def get_slm_mask_separable(
+    slm_config,
+    sensor_config,
+    crop_fact,
+    target_dim,
+    slm_vals=None,
+    slm_pattern=None,
+    deadspace=True,
+    pattern_shift=None,
+    pytorch=False,
+    device="cuda",
+    dtype=None,
+    first_color=0,
+    return_slm_vals=False,
+    requires_grad=True,
+):
+    """
+    Parameters
+    ----------`
+    slm_dim
+    slm_size
+    slm_pixel_dim
+    rpi_dim
+    rpi_pixel_dim
+    crop_fact
+    target_dim
+    slm_pattern
+    deadspace
+    pattern_shift
+    pytorch
+    device
+    dtype
+    first_color
+    Returns
+    -------
+    """
+    if dtype is None:
+        if pytorch:
+            dtype = torch.float32
+        else:
+            dtype = np.float32
+
+    overlapping_mask_size, overlapping_mask_dim, n_active_slm_pixels = get_active_pixel_dim(
+        sensor_config=sensor_config,
+        sensor_crop=crop_fact,
+        slm_config=slm_config,
+    )
+
+    d1 = np.array(overlapping_mask_size) / target_dim
+    x, y = sample_points(N=target_dim, delta=d1)
+
+    # load mask pattern
+    if slm_vals is not None:
+        """use provided values"""
+        if deadspace:
+            assert np.array_equal(slm_vals[0].shape, (n_active_slm_pixels[0], 1))
+            assert np.array_equal(slm_vals[1].shape, (1, n_active_slm_pixels[1]))
+        else:
+            assert np.array_equal(slm_vals[0].shape, (overlapping_mask_dim[0], 1))
+            assert np.array_equal(slm_vals[1].shape, (1, overlapping_mask_dim[1]))
+
+        if torch.is_tensor(slm_vals[0]) and torch.is_tensor(slm_vals[1]):
+            pytorch = True
+            device = slm_vals[0].device
+        else:
+            pytorch = False
+
+    elif slm_pattern is not None:
+        raise NotImplementedError
+
+    else:
+        """randomly generate"""
+        if deadspace:
+            _slm_vals_col = np.random.rand(*(n_active_slm_pixels[0], 1)).astype(np.float32)
+            _slm_vals_row = np.random.rand(*(1, n_active_slm_pixels[1])).astype(np.float32)
+            slm_vals = [_slm_vals_col, _slm_vals_row]
+
+        else:
+            _slm_vals_col = np.random.rand(*(overlapping_mask_dim[0], 1)).astype(np.float32)
+            _slm_vals_row = np.random.rand(*(1, overlapping_mask_dim[1])).astype(np.float32)
+            slm_vals = [_slm_vals_col, _slm_vals_row]
+
+    # create mask
+    if deadspace:
+        if torch.is_tensor(slm_vals[0]):
+            _mask_col = torch.zeros((3, y.shape[0], 1), dtype=dtype, device=device)
+            _mask_row = torch.zeros((3, 1, x.shape[1]), dtype=dtype, device=device)
+
+        else:
+            _mask_col = np.zeros((3, y.shape[0], 1), dtype=np.float32)
+            _mask_row = np.zeros((3, 1, x.shape[1]), dtype=np.float32)
+
+            if pytorch:
+                slm_vals[0] = torch.tensor(
+                    slm_vals[0], dtype=dtype, device=device, requires_grad=requires_grad
+                )
+                slm_vals[1] = torch.tensor(
+                    slm_vals[1], dtype=dtype, device=device, requires_grad=requires_grad
+                )
+                _mask_col = torch.tensor(_mask_col, dtype=dtype, device=device)
+                _mask_row = torch.tensor(_mask_row, dtype=dtype, device=device)
+
+        centers, cf = get_centers_separable(
+            n_active_slm_pixels,
+            pixel_pitch=slm_config[SLMParam.PITCH],
+            return_color_filter=True,
+            first_color=first_color,
+        )
+        if return_slm_vals:
+            print("test")
+            if pytorch:
+                cf[0] = torch.tensor(cf[0]).to(slm_vals[0])
+                cf[1] = torch.tensor(cf[1]).to(slm_vals[1])
+            return (cf[0] * slm_vals[0], cf[1] * slm_vals[1]), centers
+
+        _height_pixel, _width_pixel = (slm_config[SLMParam.CELL_SIZE] / d1).astype(int)
+
+        # Make mask_col
+        for i, _center in enumerate(centers[0][:, 0]):
+
+            _center_top_pixel = (_center / d1[0] + target_dim[0] / 2).astype(int) - np.floor(
+                _height_pixel / 2
+            ).astype(int)
+
+            # Let the possibility to have non-RGB filter
+            _rect = np.tile(cf[0][:, i][:, np.newaxis], (1, _height_pixel, 1))
+
+            if pytorch:
+                _rect = torch.tensor(_rect).to(slm_vals[0])
+
+            _mask_col[:, _center_top_pixel : _center_top_pixel + _height_pixel] = (
+                slm_vals[0][i, 0] * _rect
+            )
+
+        # Make mask_row
+        for i, _center in enumerate(centers[1][0, :]):
+
+            _center_left_pixel = (
+                (_center / d1[1] + target_dim[1] / 2).astype(int)
+                - np.floor(_width_pixel / 2).astype(int)
+                + 1
+            )
+            # Let the possibility to have non-RGB filter
+            _rect = np.tile(cf[1][:, 0, i][:, np.newaxis, np.newaxis], (1, 1, _width_pixel))
+
+            if pytorch:
+                _rect = torch.tensor(_rect).to(slm_vals[1])
+
+            _mask_row[:, :, _center_left_pixel : _center_left_pixel + _width_pixel] = (
+                slm_vals[1][0, i] * _rect
+            )
+
+    else:
+
+        _mask_col = np.zeros((3, overlapping_mask_dim[0], 1), dtype=np.float32)
+        _mask_row = np.zeros((3, 1, overlapping_mask_dim[1]), dtype=np.float32)
+
+        for i in range(n_active_slm_pixels[0]):
+            _mask_col[(i + first_color) % 3, n_active_slm_pixels[0] - 1 - i] = 1
+        for i in range(n_active_slm_pixels[1]):
+            _mask_row[:, :, : n_active_slm_pixels[1]] = 1
+
+        shift = ((np.array(overlapping_mask_dim) - np.array(n_active_slm_pixels)) / 2).astype(int)
+        _mask_col = np.roll(_mask_col, shift=shift[0], axis=1)
+        _mask_row = np.roll(_mask_row, shift=shift[1], axis=2)
+
+        if pytorch:
+            _mask_col = torch.tensor(_mask_col.astype(np.float32), dtype=dtype, device=device)
+            _mask_row = torch.tensor(_mask_row.astype(np.float32), dtype=dtype, device=device)
+
+            slm_vals[0] = torch.tensor(
+                slm_vals[0], dtype=dtype, device=device, requires_grad=requires_grad
+            )
+            slm_vals[1] = torch.tensor(
+                slm_vals[1], dtype=dtype, device=device, requires_grad=requires_grad
+            )
+
+            _mask_col *= slm_vals[0]
+            _mask_row *= slm_vals[1]
+
+            _mask_col = F.interpolate(
+                _mask_col.unsqueeze(0),
+                size=(target_dim[0], 1),
+                mode="nearest",
+            ).squeeze(0)
+            _mask_row = F.interpolate(
+                _mask_row.unsqueeze(0),
+                size=(1, target_dim[1]),
+                mode="nearest",
+            ).squeeze(0)
+
+        else:
+            _mask_col *= slm_vals[0]
+            _mask_row *= slm_vals[1]
+
+            _mask_col = cv2.resize(
+                np.transpose(_mask_col, (1, 2, 0)),
+                dsize=(1, target_dim[0]),
+                interpolation=cv2.INTER_NEAREST,
+            )
+            _mask_row = cv2.resize(
+                np.transpose(_mask_row, (1, 2, 0)),
+                dsize=(target_dim[1], 1),
+                interpolation=cv2.INTER_NEAREST,
+            )
+
+            _mask_col = np.transpose(_mask_col, (2, 0, 1))
+            _mask_row = np.transpose(_mask_row, (2, 0, 1))
+
+    return [_mask_col, _mask_row]
