4f9a05657f12f2a444eaeb81ea82526aee824e50
Eric Bezzam <ebezzam@gmail.com>
2023-11-20 14:16:10 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="waveprop",
-    version="0.0.8",
+    version="0.0.9",
     author="Eric Bezzam",
     author_email="ebezzam@gmail.com",
     description="Functions and scripts to simulate free-space optical wave propagation.",
